5093dced0c7cd138d525f4b8923121b49f29b92e
Jelegummykub <jelegummy.jrs@gmail.com>
2025-12-25 21:38:31 +0700

    feat : landing page

--- apps/client/src/components/Navbar/index.tsx
+++ apps/client/src/components/Navbar/index.tsx
@@ -1,65 +1,79 @@
 import Link from 'next/link'
 import { signOut, useSession } from 'next-auth/react'
-import { USER_ROUTES } from '../Sidebar/constants'
-import { LogOut, Waves } from 'lucide-react'
+import { LogOut, Phone, CircleUserRound } from 'lucide-react'
 import React from 'react'
-import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 const Navbar = () => {
   const { data: session } = useSession()
-  const pathname = usePathname()
 
   return (
     <>
-      <header className="flex h-auto flex-col gap-2 rounded-b-2xl bg-[#1c1d54]">
+      <div className="flex h-auto flex-col gap-2">
         <div className="mt-2 flex items-center justify-between p-2 px-5">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center">
             <div className="rounded-lg bg-white/10 p-2 backdrop-blur-sm">
-              <Waves className="h-8 w-8 text-white" />
+              <Image
+                src="/learnify-logo.png"
+                alt="Learnify Logo"
+                width={50}
+                height={50}
+              />
             </div>
             <div className="text-center">
-              <h1 className="text-xl font-bold text-white">FloodSim Viewer</h1>
+              <h1 className="text-xl font-bold text-black">Learnify</h1>
             </div>
           </div>
-          <div className="flex h-auto flex-row gap-5 px-24 py-3">
-            {USER_ROUTES.map((item, index) => (
-              <Link
-                key={index}
-                href={item.route}
-                className={`hover:text-shadow-white flex items-center justify-center gap-2 text-lg font-normal text-white hover:shadow-md ${
-                  pathname === item.route ? 'border-b border-white' : ''
-                }`}
-              >
-                <span className="flex h-5 w-5 items-center justify-center text-white">
-                  {item.icon}
-                </span>
-                <span className="">{item.title}</span>
-              </Link>
-            ))}
-          </div>
-          <div className="flex w-40 justify-end">
-            {session?.user ? (
-              <>
-                <button
-                  className="group flex h-10 w-10 items-center justify-center overflow-hidden rounded-lg bg-white text-lg font-semibold transition-all duration-700 hover:w-40"
-                  onClick={() => signOut()}
-                >
-                  <LogOut className="h-5 w-5 text-gray-800 transition-all duration-1000 group-hover:mr-2" />
-                  <span className="w-0 whitespace-nowrap opacity-0 transition-all duration-1000 group-hover:w-auto group-hover:opacity-100">
-                    ออกจากระบบ
-                  </span>
-                </button>
-              </>
-            ) : (
-              <Link href="/login">
-                <button className="btn btn-sm rounded-2xl border-[#000000] bg-[#ffffff] placeholder:bg-[#854C2F]">
-                  Login
-                </button>
-              </Link>
-            )}
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex justify-end">
+              {session?.user ? (
+                <>
+                  <button
+                    className="group flex h-10 w-10 items-center justify-center overflow-hidden rounded-lg bg-white text-lg font-semibold transition-all duration-700 hover:w-40"
+                    onClick={() => signOut()}
+                  >
+                    <LogOut className="h-5 w-5 text-gray-800 transition-all duration-1000 group-hover:mr-2" />
+                    <span className="w-0 whitespace-nowrap opacity-0 transition-all duration-1000 group-hover:w-auto group-hover:opacity-100">
+                      ออกจากระบบ
+                    </span>
+                  </button>
+                </>
+              ) : (
+                <Link href="/login">
+                  <div className="flex w-36 justify-center">
+                    <button className="flex items-center gap-2 rounded-2xl bg-white px-4 py-2 text-black">
+                      <CircleUserRound className="h-4 w-4" />
+                      ลงชื่อเข้าใช้
+                    </button>
+                  </div>
+                </Link>
+              )}
+            </div>
+            <div className="flex justify-end">
+              {session?.user ? (
+                <>
+                  <button
+                    className="group flex h-10 w-10 items-center justify-center overflow-hidden rounded-lg bg-white text-lg font-semibold transition-all duration-700 hover:w-40"
+                    onClick={() => signOut()}
+                  >
+                    <LogOut className="h-5 w-5 text-gray-800 transition-all duration-1000 group-hover:mr-2" />
+                    <span className="w-0 whitespace-nowrap opacity-0 transition-all duration-1000 group-hover:w-auto group-hover:opacity-100">
+                      ออกจากระบบ
+                    </span>
+                  </button>
+                </>
+              ) : (
+                <Link href="/login">
+                  <button className="btn btn-sm rounded-2xl bg-[#2460E3] text-white placeholder:bg-[#854C2F]">
+                    <Phone className="h-4 w-4" />
+                    ติดต่อเรา
+                  </button>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
-      </header>
+      </div>
     </>
   )
 }
--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,13 +1,45 @@
-import Link from 'next/link'
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
-        <Link href="/login">Login ไอ้ตูดหมึก</Link>
-        <Link href="/register" className="ml-4">
-          Register ไอ้ตูดหมึก
-        </Link>
+      <Navbar />
+      <div className="mt-20 flex flex-col items-center justify-center gap-14 px-4 md:mt-32">
+        <div className="flex w-full flex-col items-center justify-center gap-6">
+          <div className="flex flex-row items-center justify-center">
+            <Image
+              src="/learnify-logo.png"
+              alt="Learnify Logo"
+              width={50}
+              height={50}
+            />
+            <h1 className="text-xl font-bold text-black">Learnify</h1>
+          </div>
+
+          <div className="text-5xl font-bold">ยินดีต้อนรับสู่ Learnify</div>
+          <div className="text-5xl font-bold text-[#1F57CF]">
+            เว็บไซต์ที่ทำให้ การเรียนรู้ไม่เหมือนเดิม
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-xl">
+              ค้นพบประสบการณ์การเรียนรูปแบบใหม่ ที่เทคโนโลยีช่วยให้บทเรียนสนุก
+              เข้าใจง่าย
+            </p>
+            <p className="text-xl">
+              และมีปฏิสัมพันธ์มากขึ้น ทั้งในและนอกห้องเรียน
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full flex-row items-center justify-center gap-2">
+          <button className="btn btn-md w-48 rounded-full bg-[#2460E3] text-white">
+            เริ่มต้นใช้งาน
+          </button>
+
+          <button className="btn btn-md w-48 rounded-full bg-white text-[#2460E3]">
+            ลงชื่อเข้าใช้
+          </button>
+        </div>
       </div>
     </>
   )
