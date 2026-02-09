ac72a6d31e0676d1d784662f339e14af60852f98
Jelegummykub <jelegummy.jrs@gmail.com>
2026-02-09 22:13:18 +0700

    fix

--- apps/server/src/api/character/internal/internal.dto.ts
+++ apps/server/src/api/character/internal/internal.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export class CreateCharacterDto extends createZodDto(
     z.object({
-        name: z.string().min(1),
+        name: z.string().optional(),
         description: z.string().optional(),
         modelUrl: z.string().optional(),
         imageUrl: z.string().optional(),
