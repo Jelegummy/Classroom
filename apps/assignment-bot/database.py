e55c2b5e4208a4b408c7254ea10edb2871040232
Jelegummykub <jelegummy.jrs@gmail.com>
2026-03-30 23:21:47 +0700

    fix : config

--- apps/assignment-bot/database.py
+++ apps/assignment-bot/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = "postgresql://local:local@127.0.0.1:5436/classroom"
 
 
 engine = create_engine(DATABASE_URL)
